3324e80b204f109e2e6e41a2cc46c4f70a65d8ea
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-16 15:52:59 -0300

    Create the javascript, html and css cards

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 import CardGame from "../CardGame";
 
-function CardFrontBack() {
+function CardFrontBack(icon, altIcon) {
     window.cardFrontBack = {};
     window.cardFrontBack.handleClick = (event) => {
         const $origin = event.target;
@@ -16,7 +16,7 @@ function CardFrontBack() {
                 ${CardGame()}
             </div>
             <div class="card -back">
-                ${CardGame("javascript", "Logo do Java Script")}
+                ${CardGame(icon, altIcon)}
             </div>
         </article>
     `;
